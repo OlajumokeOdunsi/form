44262be1089b7fb1150f9f231033178e997ddfe9
Jum-jum-maker <80886533+OlajumokeOdunsi@users.noreply.github.com>
2024-04-26 09:18:52 +0100

    updated with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Form from './Form'
-
+import FormD from './FormD'
+import Getformdata from './Getformdata'
 function App() {
+  const handleChange = (event) => {
+  };
+
 
   return (
     <>
-    <Form/>
-    </>
+    <FormD handleChange={handleChange}/>
+  <Getformdata/>
+ </>
   )
 }
 
